b665a3fffc6e88e591442720c1a06ee97ba5964d
MMrhassel <48781151+MMrhassel@users.noreply.github.com>
2019-12-26 14:38:45 +0100

    Update save.py

--- modules/save.py
+++ modules/save.py
@@ -4,10 +4,11 @@ def Save(URL,SOURCE):
 	try:
 	    FileName = md5(b"URL").hexdigest()
 	    try:
-	        os.mkdir("out/"+URL)
-	        with open(os.getcwd()+"/out/"+URL+"/"+FileName,'a',encoding="utf-8") as open_file:
+                URL_FOLDER = re.sub(r'https://www\.|http://www\.|https://www|http://|https://|www\.', "", URL).strip('/')
+	        os.mkdir("out/"+URL_FOLDER)
+	        with open("out/"+URL_FOLDER+"/"+FileName,'a+',encoding="utf-8") as open_file:
 	    	    open_file.write(SOURCE)
-	    except:
-	    	pass
+	    except Exception as error:
+	    	print(error)
 	except Exception as error:
 		print(error)
