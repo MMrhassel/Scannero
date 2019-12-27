2b198ef5a0c3056c08ddd9b7706423c43b358f47
MMrhassel <48781151+MMrhassel@users.noreply.github.com>
2019-12-27 10:54:46 +0100

    Update save.py

--- modules/save.py
+++ modules/save.py
@@ -1,14 +1,19 @@
 from hashlib import md5
 import os
-def Save(URL,SOURCE):
-	try:
-	    FileName = md5(b"URL").hexdigest()
-	    try:
-                URL_FOLDER = re.sub(r'https://www\.|http://www\.|https://www|http://|https://|www\.', "", URL).strip('/')
-	        os.mkdir("out/"+URL_FOLDER)
-	        with open("out/"+URL_FOLDER+"/"+FileName,'a+',encoding="utf-8") as open_file:
-	    	    open_file.write(SOURCE)
-	    except Exception as error:
-	    	print(error)
-	except Exception as error:
-		print(error)
+import re
+
+
+def Save(URL, SOURCE):
+    try:
+        FileName = md5(b"URL").hexdigest()
+        try:
+            URL_FOLDERX = re.sub(
+                r'https://www\.|http://www\.|https://www|http://|https://|www\.', "", URL)
+            URL_Folder = URL_FOLDERX
+            os.mkdir("out/"+URL_Folder)
+            with open("out/"+URL_Folder+"/"+FileName, 'a+', encoding="utf-8") as open_file:
+                open_file.write(SOURCE)
+        except:
+            pass
+    except:
+        pass
